73f10bbdf7066180abf2c6544ef03a2aab9f0339
Liyang <blanktiger@qq.com>
2018-08-15 15:58:36 +0800

    Create webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,73 @@
+const path = require('path');
+const webpack = require('webpack');
+const htmlWebpackPlugin = require("html-webpack-plugin");
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const OpenBrowserPlugin = require('open-browser-webpack-plugin');
+const base = {
+	resolve: {
+		modules: [
+			path.resolve(__dirname, 'node_modules'),
+      path.join(__dirname, './src')
+		],
+		alias: {
+			src: path.resolve(__dirname, 'src'),
+		}
+	},
+
+	module: {
+		rules: [
+			{test: /\.(js|jsx)$/, use: 'babel-loader', exclude: /node_modules/},
+			/*{
+        test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+        use: ['file-loader?limit=1000&name=assets/images/[md5:hash:base64:10].[ext]']
+      },*/
+		]
+	},
+	devtool: 'cheap-module-eval-source-map',
+  stats: {
+    colors: true,
+    timings: true,
+		version:true,
+	  hash:true,
+    chunks: true,
+    chunkModules: false,
+    cached: true,
+    cachedAssets: true
+  }
+};
+
+const { platform } = process;
+let browser = 'google-chrome';
+if (platform === 'darwin') {
+  browser = 'google chrome';
+} else if (platform === 'win32') {
+  browser = 'chrome';
+}
+
+let hotPolyfill = ['react-hot-loader/patch','react-dev-utils/webpackHotDevClient','webpack/hot/only-dev-server'];
+module.exports=Object.assign({
+	mode:"development",
+	entry:{
+    vendor: ['react', 'react-dom'],
+		todo:[...hotPolyfill,"./example/todo/index.js"]
+	},
+	output:{
+		filename:"[name].js?v=[hash:8]",
+		path:path.resolve(__dirname,"dist"),
+		publicPath: "./"
+	},
+
+	plugins:[
+		new CleanWebpackPlugin(['dist']),
+    new webpack.HotModuleReplacementPlugin(),
+		new htmlWebpackPlugin({
+			title: "todo example",
+			template: "./template/index.html",
+			filename: "index.html",
+			inject: "body",
+			chunks: ["vendor","todo"]
+		}),
+    new OpenBrowserPlugin({ browser, url: 'http://localhost:9009' })
+	]
+},base);
+
